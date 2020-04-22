d4adca8cbb370d1334e3d6b425a94f2c67fd44ca
Carly Langlois <cj.langlois.cl@gmail.com>
2020-04-22 11:54:29 -0500

    put index form into template

--- SpaDay/Controllers/SpaController.cs
+++ SpaDay/Controllers/SpaController.cs
@@ -54,28 +54,9 @@ namespace SpaDay.Controllers
             }
         }
 
-        // GET: /<controller>/
         public IActionResult Index()
         {
-            string html = "<form method = 'post'>" +
-                        "Name: <br>" +
-                        "<input type = 'text' name = 'name'>" +
-                        "<br>Skin type: <br>" +
-                        "<select name = 'skintype'>" +
-                        "<option value = 'oily'>Oily</option>" +
-                        "<option value = 'combination'>Combination</option>" +
-                        "<option value = 'normal'>Normal</option>" +
-                        "<option value = 'dry'>Dry</option>" +
-                        "</select><br>" +
-                        "Manicure or Pedicure? <br>" +
-                        "<select name = 'manipedi'>" +
-                        "<option value = 'manicure'>Manicure</option>" +
-                        "<option value = 'pedicure'>Pedicure</option>" +
-                        "</select><br>" +
-                        "<input type = 'submit' value = 'Submit'>" +
-                        "</form>";
-
-            return Content(html, "text/html");
+            return View();
         }
 
         [HttpPost]
