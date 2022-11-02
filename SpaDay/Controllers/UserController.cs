00acd544cc3f301e5ce21a05af03a3b4705a08b8
zkrem56 <zkremer702@gmail.com>
2022-11-02 14:04:30 -0500

    Committing previous changes from last time

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /user
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /user/add
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // POST: /user
+        [HttpPost("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // Check the password to match verify
+            if (newUser.Password.Equals(verify))
+            {
+                ViewBag.user = newUser;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Passwords do Not Match";
+                ViewBag.name = newUser.Name;
+                ViewBag.username = newUser.Username;
+                ViewBag.email = newUser.Email;
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; }
+
+        public User () 
+        {
+            Date = DateTime.Now;
+        }
+
+        public User (string username, string name, string password, string email): this()
+        {
+            Username = username;
+            Name = name;
+            Password = password;
+            Email = email;
+        }
+    }
+}
